b4761bc139db60cbfc855fbf7146efc0db517de7
andersonlucas94 <42416035+andersonlucas94@users.noreply.github.com>
2018-08-15 14:01:15 -0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Auth::routes();
+
+/*
+|------------------------------------------------------------------------------------
+| Admin
+|------------------------------------------------------------------------------------
+*/
+Route::group(['prefix' => ADMIN, 'as' => ADMIN . '.', 'middleware'=>['auth', 'Role:0']], function() {
+    Route::get('/', 'DashboardController@index')->name('dash');
+    Route::resource('users', 'UserController');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
